7ad73a94e5b770099497a6de3c36a86e0dd52f1b
Sara Rojas Martinez <s.rojas12@uniandes.edu.co>
2025-08-12 11:08:00 +0300

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -13,7 +13,7 @@ export default function Home() {
       <main>
         <Header title="Welcome to my app!" />
         <p className="description">
-          Get started by editing <code>pages/index.js</code>
+          Go to https://sararoma95.github.io/sr/ <code>pages/index.js</code>
         </p>
       </main>
 
